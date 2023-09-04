d551f6235a6de05a8590c1bb52b5ca7465408a8d
ksh <test@test.123>
2023-09-04 17:05:42 +0900

    spring_Quiz02

---
+++ src/main/java/com/care/test02/PrintClass.java
@@ -0,0 +1,8 @@
+package com.care.test02;
+
+public class PrintClass {
+   
+   public void print(int num1, int num2, String op, int result) {
+      System.out.println(num1+op+num2+"="+result);
+   }
+}
